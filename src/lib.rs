13b230ced516f4a57717d88075d5b1d25857b490
andrew <>
2023-09-02 23:45:44 +0900

    More methods, renames

--- src/lib.rs
+++ src/lib.rs
@@ -27,22 +27,16 @@ pub fn scope_any<T: 'static>(value: T) -> Box<dyn ScopeValue> {
     return Box::new(_ScopeValue(value));
 }
 
-struct _ScopeElement {
+struct El_ {
     el: Element,
-    parent: Option<Weak<RefCell<_ScopeElement>>>,
+    parent: Option<Weak<RefCell<El_>>>,
     index_in_parent: usize,
-    children: Vec<ScopeElement>,
+    children: Vec<El>,
     local: Vec<Box<dyn ScopeValue>>,
 }
 
-impl _ScopeElement {
-    fn splice(
-        &mut self,
-        self2: &Rc<RefCell<_ScopeElement>>,
-        offset: usize,
-        remove: usize,
-        add: Vec<ScopeElement>,
-    ) {
+impl El_ {
+    fn splice(&mut self, self2: &Rc<RefCell<El_>>, offset: usize, remove: usize, add: Vec<El>) {
         let el_children = self.el.children();
 
         // Remove existing dom children
@@ -77,7 +71,7 @@ impl _ScopeElement {
         self.children.clear();
     }
 
-    fn extend(&mut self, self2: &Rc<RefCell<_ScopeElement>>, add: Vec<ScopeElement>) {
+    fn extend(&mut self, self2: &Rc<RefCell<El_>>, add: Vec<El>) {
         let offset = self.children.len();
         for (i, child) in add.iter().enumerate() {
             let mut c = child.0.borrow_mut();
@@ -98,9 +92,9 @@ impl _ScopeElement {
 /// a child element you'll end up with a reference cycle and the subtree will never
 /// be freed.  You can use `weak()` to get a weak reference if you want to do this.
 #[derive(Clone)]
-pub struct ScopeElement(Rc<RefCell<_ScopeElement>>);
+pub struct El(Rc<RefCell<El_>>);
 
-impl ScopeElement {
+impl El {
     pub fn text(self, text: &str) -> Self {
         self.0.borrow().el.set_text_content(Some(text));
         return self;
@@ -130,6 +124,12 @@ impl ScopeElement {
         return self;
     }
 
+    /// Remove an attribute from the element.
+    pub fn mut_remove_attr(&self, key: &str) -> &Self {
+        self.0.borrow().el.remove_attribute(key).unwrap_throw();
+        return self;
+    }
+
     pub fn classes(self, keys: &[&str]) -> Self {
         let c = self.0.borrow().el.class_list();
         for k in keys {
@@ -156,30 +156,30 @@ impl ScopeElement {
         return self;
     }
 
-    pub fn push(self, add: ScopeElement) -> Self {
+    pub fn push(self, add: El) -> Self {
         self.0.borrow_mut().extend(&self.0, vec![add]);
         return self;
     }
 
     /// Add a single element to the end.
-    pub fn mut_push(&self, add: ScopeElement) -> &Self {
+    pub fn mut_push(&self, add: El) -> &Self {
         self.0.borrow_mut().extend(&self.0, vec![add]);
         return self;
     }
 
-    pub fn extend(self, add: Vec<ScopeElement>) -> Self {
+    pub fn extend(self, add: Vec<El>) -> Self {
         self.0.borrow_mut().extend(&self.0, add);
         return self;
     }
 
     /// Add multiple elements to the end.
-    pub fn mut_extend(&self, add: Vec<ScopeElement>) -> &Self {
+    pub fn mut_extend(&self, add: Vec<El>) -> &Self {
         self.0.borrow_mut().extend(&self.0, add);
         return self;
     }
 
     /// Add and remove multiple elements.
-    pub fn mut_splice(&self, offset: usize, remove: usize, add: Vec<ScopeElement>) -> &Self {
+    pub fn mut_splice(&self, offset: usize, remove: usize, add: Vec<El>) -> &Self {
         self.0.borrow_mut().splice(&self.0, offset, remove, add);
         return self;
     }
@@ -190,14 +190,14 @@ impl ScopeElement {
         return self;
     }
 
-    pub fn drop<T: 'static>(self, supplier: impl FnOnce(&ScopeElement) -> T) -> Self {
+    pub fn own<T: 'static>(self, supplier: impl FnOnce(&El) -> T) -> Self {
         self.0.borrow_mut().local.push(Box::new(_ScopeValue(supplier(&self))));
         return self;
     }
 
     /// Attach the value to this scope, so it doesn't get dropped until the element is
     /// removed from the tree.
-    pub fn mut_drop<T: 'static>(&self, local: T) -> &Self {
+    pub fn mut_own<T: 'static>(&self, local: T) -> &Self {
         self.0.borrow_mut().local.push(Box::new(_ScopeValue(local)));
         return self;
     }
@@ -224,20 +224,24 @@ impl ScopeElement {
     pub fn weak(&self) -> WeakScopeElement {
         return WeakScopeElement(Rc::downgrade(&self.0));
     }
+
+    pub fn unwrap(&self) -> Element {
+        return self.0.as_ref().borrow().el.clone();
+    }
 }
 
 #[derive(Clone)]
-pub struct WeakScopeElement(Weak<RefCell<_ScopeElement>>);
+pub struct WeakScopeElement(Weak<RefCell<El_>>);
 
 impl WeakScopeElement {
-    pub fn upgrade(&self) -> Option<ScopeElement> {
-        return Some(ScopeElement(self.0.upgrade()?));
+    pub fn upgrade(&self) -> Option<El> {
+        return Some(El(self.0.upgrade()?));
     }
 }
 
 /// Create a new element.
-pub fn el(tag: &str) -> ScopeElement {
-    return ScopeElement(Rc::new(RefCell::new(_ScopeElement {
+pub fn el(tag: &str) -> El {
+    return El(Rc::new(RefCell::new(El_ {
         el: document().create_element(tag).unwrap(),
         parent: None,
         index_in_parent: 0,
@@ -248,8 +252,8 @@ pub fn el(tag: &str) -> ScopeElement {
 
 /// Create a new scoped element from an element passed in (ex: for existing
 /// elements, or namespaced elements set up specially).
-pub fn el_from_raw(el: Element) -> ScopeElement {
-    return ScopeElement(Rc::new(RefCell::new(_ScopeElement {
+pub fn el_from_raw(el: Element) -> El {
+    return El(Rc::new(RefCell::new(El_ {
         el: el,
         parent: None,
         index_in_parent: 0,
@@ -259,19 +263,19 @@ pub fn el_from_raw(el: Element) -> ScopeElement {
 }
 
 thread_local!{
-    static ROOT: Cell<Vec<ScopeElement>> = Cell::new(vec![]);
+    static ROOT: Cell<Vec<El>> = Cell::new(vec![]);
 }
 
 /// Replaces the existing element with id `id`, taking ownership and extending the
 /// new element's lifetime.
-pub fn set_root_replace(id: &str, el: ScopeElement) {
+pub fn set_root_replace(id: &str, el: El) {
     document().get_element_by_id(id).unwrap_throw().replace_with_with_node_1(&el.0.borrow().el).unwrap_throw();
     ROOT.with(|r| r.set(vec![el]));
 }
 
 /// Sets the elements as the children of the body, taking ownership and their
 /// lifetimes.
-pub fn set_root(elements: Vec<ScopeElement>) {
+pub fn set_root(elements: Vec<El>) {
     document()
         .body()
         .unwrap_throw()
