b0063f5eaec0cb2b1d26d62a5d5254deca387913
Andrew Baxter <>
2025-11-01 20:53:08 +0900

    Allow accessing spawn_rooted's return value

--- src/spawn.rs
+++ src/spawn.rs
@@ -1,32 +1,31 @@
-use futures::{
-    channel::oneshot::channel,
-    select,
-    Future,
-    FutureExt,
-};
-use wasm_bindgen_futures::spawn_local;
-use crate::{
-    own::{
-        ScopeValue,
-        defer,
+use {
+    futures::{
+        Future,
+        FutureExt,
+        channel::oneshot::{
+            Receiver,
+            channel,
+        },
+        select,
     },
+    wasm_bindgen_futures::spawn_local,
 };
 
 /// Spawn a background task that's canceled when the returned scope value is
 /// dropped. You can use this to attach background tasks to elements that are
 /// stopped when the element is removed.
-pub fn spawn_rooted(f: impl Future<Output = ()> + 'static) -> ScopeValue {
-    let (cancel_tx, mut cancel_rx) = channel();
+pub fn spawn_rooted<T: 'static>(f: impl Future<Output = T> + 'static) -> Receiver<T> {
+    let (mut complete_tx, complete_rx) = channel();
     let f = Box::pin(f);
     spawn_local(async move {
+        let cancel_rx = complete_tx.cancellation();
         select!{
-            _ = cancel_rx => {
+            _ = cancel_rx.fuse() => {
             },
-            _ = f.fuse() => {
+            r = f.fuse() => {
+                _ = complete_tx.send(r);
             }
         }
     });
-    return defer(move || {
-        _ = cancel_tx.send(());
-    });
+    return complete_rx;
 }
